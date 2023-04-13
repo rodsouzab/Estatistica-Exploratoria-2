7e9ed24f2840960152d447ac1b7a5f8a007f0b71
rodsouzab <82851436+rodsouzab@users.noreply.github.com>
2023-04-13 20:30:23 -0300

    Update and rename mapas.R to NovoApp.R

--- NovoApp.R
+++ NovoApp.R
@@ -5,9 +5,6 @@ library(tidyverse)
 library(readxl)
 library(sp)
 
-
-
-
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
@@ -45,11 +42,15 @@ ui <- fluidPage(
                            tableOutput('table'),
                            plotOutput('hist')
                   ),
-                  tabPanel("Intervalo de Confiança", sliderInput(
-                    "Nível de Confiança", "Selecione o Nível de Confiança",
-                    min = 0, max = 1, value = 0.5
-                  )),
-                  tabPanel("Regressão", plotOutput('reg'))
+                  tabPanel("Intervalo de Confiança", 
+                           sliderInput(
+                    "intervalo", "Selecione o Nível de Confiança: ",
+                    min = 90, max = 99.9, value = 95, post = '%'
+                  ),tableOutput('table2')),
+                  tabPanel("Regressão", plotOutput('reg'),
+                           tableOutput('table3'),
+                           tableOutput('table4'),
+                           tableOutput('table5'))
       )
       
     )
@@ -103,17 +104,6 @@ server <- function(input, output) {
   
   ################################################################################
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   df2 = excel.data %>% separate(Hora, c("dia", "hora"), " ") %>% 
     separate(hora, c("horas","minutos","segundos"), ":")
   
@@ -130,20 +120,15 @@ server <- function(input, output) {
   x = df2_new %>% select(veloc)
   
   
-  n = reactive(length(x()))
+  n = reactive(length(x$veloc))
   
-  xbarra = reactive(mean(x()))
+  xbarra = reactive(mean(x$veloc))
   
-  sig = sd(sig2)
-  
-  sig_xbar = reactive(sig()/sqrt(n()))
+  sig_xbar = reactive(sqrt(sig2())/sqrt(n()))
   
   mu0 = reactive({
     as.integer(input$mu0)
   })
-  
-  
-  
   
   alfa = reactive(as.numeric(input$alfa))
   
@@ -152,11 +137,11 @@ server <- function(input, output) {
   
   p = reactive({
     if(teste() == "bi"){
-      1 - alfa() + alfa()/2
+      p = 1 - alfa() + alfa()/2
     }else if(teste() == "esq"){
-      alfa()
+      p = alfa()
     }else{
-      1-alfa()
+      p = 1-alfa()
     }
   })
   
@@ -173,47 +158,91 @@ server <- function(input, output) {
   
   output$table <- renderTable(
     if(teste() == "bi" & zcalc() < ztab() & zcalc() > -ztab() |
-       teste() == "esq" & zcalc() < ztab() |
-       teste() == "dir" & zcalc() > ztab()
+       teste() == "esq" & zcalc() > ztab() |
+       teste() == "dir" & zcalc() < ztab()
     ){
-      data.frame(Resultado = paste0('Aceitamos H0 ao nível de sig. = ', alfa()))
+      data.frame(Resultado = paste0('Aceitamos H0 ao nível de ', (alfa()*100), '%'))
     }else{
-      data.frame(Resultado = paste0('Rejeitamos H0 ao nível de sig. = ', alfa()))
+      data.frame(Resultado = paste0('Rejeitamos H0 ao nível de ', (alfa()*100), '%'))
     }
   )
   
-  
-  
   output$hist = renderPlot({
-    hist(x(), main='', freq = FALSE)
+    hist(x$veloc, main='', freq = FALSE)
     abline(v = mu0(), col= 'red')
     abline(v = xbarra(), col= 'blue')
   })
   
+  #########################################################################
+  
+  df3 = excel.data %>% separate(Hora, c("dia", "hora"), " ") %>% 
+    separate(hora, c("horas","minutos","segundos"), ":")
   
   
+  df3_new = df2 %>% filter(minutos == 45 & segundos >= 18 | minutos > 45) %>%
+    filter(minutos == 49 & segundos <= 23 | minutos < 49)
+  
+  x3 = df3_new %>% select(veloc)
+  
+  x3bar = reactive(mean(x3$veloc))
+  
+  intervalo = reactive(as.numeric(input$intervalo))
+  
+  n3 = reactive(length(x3$veloc))
+  
+  alfa3 = reactive((100-intervalo())/100)
+  
+  z = reactive(qnorm(1 - (alfa3()/2)))
+  
+  s2 = reactive(var(x3$veloc))
+  
+  sxbar = reactive(sqrt(s2())/sqrt(n3()))
   
   
+  ic_neg = reactive(x3bar() - (z()*sxbar()))
+  ic_pos = reactive(x3bar() + (z()*sxbar()))
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  output$table2 <- renderTable(
+      data.frame(Resultado = paste0('IC(ao nível de ', intervalo(), '%) = [ ',
+                                    ic_neg(), ' ; ', ic_pos(),' ]'))
+    
+  )
   
   
   #############################################################################
-  output$reg = renderPlot({
-    #gráfico de regressão com dados 'cars' aqui!
-  })
+  
+  n4 = length(cars$speed)
+  
+  x4 = cars$dist
+  y4 = cars$speed
+  num = n4*sum(x4*y4) - sum(x4)*sum(y4)
+  den1 = sqrt(n4*sum(x4^2) - sum(x4)^2)
+  den2 = sqrt(n4*sum(y4^2) - sum(y4)^2)
+  
+  r = num/(den1*den2)
+  r2 = r^2
+  
+  reta = lm(y4~x4)
+  
+  a = round(reta$coefficients[1], 2)
+  b = round(reta$coefficients[2], 2)
+ 
+  output$reg <- renderPlot(plot(cars$dist,cars$speed,pch = 16,
+                                cex = 1.3, col = "blue",xlab="Distância",
+                                ylab="Velocidade", xlim = c(0,120),
+                                ylim = c(0 , 30)) + abline(a = a, b = b))
+  
+  output$table3 <- renderTable(
+    data.frame("R" = paste0(r))
+  )
+  
+  output$table4 <- renderTable(
+    data.frame("R2" = paste0(r2))
+  )
+  
+  output$table5 <- renderTable(
+    data.frame("Equação da Reta" = paste0('y = ', b, ' + ', a , 'x'))
+  )
   
 }
 
